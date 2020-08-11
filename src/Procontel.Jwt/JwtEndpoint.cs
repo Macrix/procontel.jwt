02489733bc22b2068544c57cbe9ea1dc23220cef
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2020-08-11 14:52:20 +0200

    init jwt endpoint

---
+++ src/Procontel.Jwt/JwtEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using ProconTel.Sdk.Attributes;
+using ProconTel.Sdk.Builders;
+using ProconTel.Sdk.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Procontel.Jwt
+{
+    [EndpointMetadata(Name = "Procontel.Jwt")]
+    public class JwtEndpoint : IEndpointLifeTimeCycle
+    {
+        private readonly ILogger _logger;
+
+        public JwtEndpoint(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public Task InitializeAsync()
+        {
+            _logger.Information("Jwt initalized");
+            return Task.CompletedTask;
+        }
+
+        public Task TerminateAsync()
+        {
+            _logger.Information("Jwt terminated");
+            return Task.CompletedTask;
+        }
+    }
+}
